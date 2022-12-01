ba40546e274c8cba19815c852aaca6ff683e2016
PatriciaDivi <patriciavianad@gmail.com>
2022-12-01 19:50:05 -0300

    feat(Development): :sparkles: implementado os métodos para lidar com uma 'FILA'

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -1,15 +1,23 @@
 class Queue:
     def __init__(self):
-        """Inicialize sua estrutura aqui"""
+        self._data = []
 
     def __len__(self):
-        """Aqui irá sua implementação"""
+        if len(self._data) == 0:
+            return 0
+        return len(self._data)
 
     def enqueue(self, value):
-        """Aqui irá sua implementação"""
+        self._data.append(value)
 
     def dequeue(self):
-        """Aqui irá sua implementação"""
+        if self.__len__() is not None:
+            return self._data.pop(0)
+        return None
 
-    def search(self, index):
-        """Aqui irá sua implementação"""
+    def search(self, indexParams):     
+        if type(indexParams) != int or len(self._data)-1 < indexParams or indexParams < 0:
+            raise IndexError(f"O Index passado é inválido...")
+        for eachIndex in range(self.__len__()):
+            if indexParams == eachIndex:
+                return self._data[indexParams]
