38dcd713afa1de5b4d8191f5dbfd9e521faa6cab
PatriciaDivi <patriciavianad@gmail.com>
2022-12-02 01:25:44 -0300

    feat(Development): :sparkles: implementado os métodos para lidar com uma 'FILA'

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -15,9 +15,11 @@ class Queue:
             return self._data.pop(0)
         return None
 
-    def search(self, indexParams):     
-        if type(indexParams) != int or len(self._data)-1 < indexParams or indexParams < 0:
-            raise IndexError(f"O Index passado é inválido...")
+    def search(self, indexParams):
+        if (type(indexParams) != int or len(self._data)-1 < indexParams
+                or indexParams < 0):
+            raise IndexError("O Index passado é inválido...")
+
         for eachIndex in range(self.__len__()):
             if indexParams == eachIndex:
                 return self._data[indexParams]
