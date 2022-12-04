a27328c93af0f82cdd5a592dcce5c89a54890f74
PatriciaDivi <patriciavianad@gmail.com>
2022-12-04 16:51:04 -0300

    fix(BUGS): :bug: Correções de erro do 'LINT'

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,5 +1,6 @@
 import sys
 
+
 def txt_importer(path_file):
     try:
         if path_file.endswith('.txt'):
@@ -7,9 +8,11 @@ def txt_importer(path_file):
                 resultData = fileReading.read().splitlines()
                 return resultData
         return sys.stderr.write('Formato inválido\n')
-    except:
+    except FileNotFoundError:
         return sys.stderr.write(f'Arquivo {path_file} não encontrado\n')
 
 # splitlines é para formatar o text após '\n', E retornar em uma LISTA
-# TEXTO-SEM-splitlines()-FORMATAÇÃO = "Thank you for the music\nWelcome to the jungle"
-# TEXTO-COM-splitlines()-FORMATAÇÃO = ['Thank you for the music', 'Welcome to the jungle']
+# TEXTO-SEM-splitlines()-FORMATAÇÃO:
+# "Thank you for the music\nWelcome to the jungle"
+# TEXTO-COM-splitlines()-FORMATAÇÃO:
+# ['Thank you for the music', 'Welcome to the jungle']
