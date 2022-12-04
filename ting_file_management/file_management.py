0aacbaf09ff95e21e6037568b728138e35a3706e
PatriciaDivi <patriciavianad@gmail.com>
2022-12-04 16:40:19 -0300

    feat(Development): :sparkles: Implementado Função 'file_management'
    Fução que valida se arquivo é um '.txt', faz a leitura do '.txt', e realiza a formatação do arquivo corretamente dentro de uma 'LIST'

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,15 @@
+import sys
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    try:
+        if path_file.endswith('.txt'):
+            with open(path_file, 'r') as fileReading:
+                resultData = fileReading.read().splitlines()
+                return resultData
+        return sys.stderr.write('Formato inválido\n')
+    except:
+        return sys.stderr.write(f'Arquivo {path_file} não encontrado\n')
+
+# splitlines é para formatar o text após '\n', E retornar em uma LISTA
+# TEXTO-SEM-splitlines()-FORMATAÇÃO = "Thank you for the music\nWelcome to the jungle"
+# TEXTO-COM-splitlines()-FORMATAÇÃO = ['Thank you for the music', 'Welcome to the jungle']
