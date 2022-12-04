c2b26dbdfe620e03ee72b15f54f8d293d596d9c5
PatriciaDivi <patriciavianad@gmail.com>
2022-12-04 19:21:02 -0300

    feat(Development): :sparkles: Implementado fução 'process'
    Essa função deverá ser capaz de transformar o conteúdo da lista gerada pela função "txt_importer" em um dicionário que será armazenado dentro da "Queue".

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,20 @@
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação."""
+    for eachElement in instance._data:
+        if path_file in eachElement["nome_do_arquivo"]:
+            return None
+
+    resultTxtImporters = txt_importer(path_file)
+    resultFinished = {
+        "nome_do_arquivo": f"{path_file}",
+        "qtd_linhas": len(resultTxtImporters),
+        "linhas_do_arquivo": resultTxtImporters,
+    }
+    
+    instance.enqueue(resultFinished)
+    print(resultFinished)
 
 
 def remove(instance):
