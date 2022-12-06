e21d2c4b5399d3b922cf6dd5f316d5bd59f493f4
PatriciaDivi <patriciavianad@gmail.com>
2022-12-06 17:04:40 -0300

    feat(Development): :sparkles: Implemente uma função 'exists_word'
    Que verifique a existência de uma palavra em todos os arquivos processados.

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,19 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    resultExpect = list()
+    resultOccurrences = list()
+
+    for pos, element in enumerate(instance._data[0]["linhas_do_arquivo"]):
+        if word.lower() in element.lower():
+            resultOccurrences.append({"linha": pos + 1})
+
+    if 0 < len(resultOccurrences):
+        resultExpect.append({
+            "palavra": word,
+            "arquivo": instance._data[0]["nome_do_arquivo"],
+            "ocorrencias": resultOccurrences
+        })
+
+    return resultExpect
 
 
 def search_by_word(word, instance):
